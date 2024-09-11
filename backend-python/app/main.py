8eb085e45564e573d25412b96a29026fe67345dc
Renan <renanluis2001@outlook.com>
2024-09-11 20:17:25 -0300

    Fixes nodejs dependencies by moving source code to src folder

--- backend-python/app/main.py
+++ backend-python/app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { i: f'WOWWWWW {i}'  for i in range(100) }
+    return { i: f'Python\'s FastApi is up and running {i}'  for i in range(100) }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
