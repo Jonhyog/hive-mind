6efdff757020cc73b9e39e9864b585735d5b1e4d
Renan <renanluis2001@outlook.com>
2024-09-13 16:29:32 -0300

    Adds OpenCv to Python Container and creates simple endpoint do upload videos

---
+++ backend-python/app/beeCounting.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import tempfile
+import os
+import time
+
+async def process_video(file):
+    # Registra o tempo de início
+    start_time = time.time()
+
+    # Cria um arquivo temporário para o vídeo
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
+        temp_file.write(await file.read())
+        temp_file_path = temp_file.name
+
+    # Abre o vídeo usando OpenCV
+    cap = cv2.VideoCapture(temp_file_path)
+
+    if not cap.isOpened():
+        # Remove o arquivo temporário em caso de erro
+        os.remove(temp_file_path)
+        return {"error": "Erro ao abrir o vídeo."}
+
+    # Obtém a taxa de frames (FPS) e o número total de frames
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    # Calcula a duração do vídeo em segundos
+    duration = frame_count / fps
+
+    # Libera o objeto VideoCapture e remove o arquivo temporário
+    cap.release()
+    os.remove(temp_file_path)
+
+    # Registra o tempo de fim e calcula a duração do processamento
+    end_time = time.time()
+    processing_time = end_time - start_time
+
+    return {
+        "filename": file.filename,
+        "duration_seconds": duration,
+        "processing_time_seconds": processing_time
+    }
--- backend-python/app/main.py
+++ backend-python/app/main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+import cv2.version
+from fastapi import FastAPI, File, UploadFile
+from beeCounting import process_video
+import cv2 
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { i: f'Python\'s FastApi is up and running {i}'  for i in range(100) }
+    return {
+        "response":"Python's Server is Up and running!!",
+        "OpenCv-Version":cv2.__version__
+            }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/upload-video")
+async def read_video(file: UploadFile = File(...)):
+    return await process_video(file)
